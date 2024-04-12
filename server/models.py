a42d52e9fd6d8192e3ab3594c27be74eb3c7b4a3
Immanuel <immanuelcherui1@gmail.com>
2024-04-12 10:20:34 +0300

    Done with assignment

--- server/models.py
+++ server/models.py
@@ -10,23 +10,59 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# Association table to store many-to-many relationship between employees and meetings
+employee_meetings = db.Table(
+    'employees_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+)
 
-class Customer(db.Model):
+
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates='customer')
+
+    # Association proxy 
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
+
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
